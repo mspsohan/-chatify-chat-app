99a95b8a115c74bb476ccfde82d5b2eb101ffb10
mspsohan <mspsohan@gmail.com>
2024-02-16 04:32:00 +0600

    Fix some error on Query Data

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,8 @@ app.use(cors({
    optionsSuccessStatus: 200,
 }))
 
+connectDB()
+
 app.get("/", (req, res) => {
    res.send("Api is Running")
 })
@@ -34,13 +36,49 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+const PORT = process.env.PORT;
 
+const server = app.listen(
+   PORT,
+   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
 
-const main = () => {
-   connectDB()
-   app.listen(process.env.PORT, () => {
-      console.log(`Server running on port: ${process.env.PORT}`)
-   })
-}
+const io = require("socket.io")(server, {
+   pingTimeout: 60000,
+   cors: {
+      origin: "http://localhost:5173",
+      // credentials: true,
+   },
+});
 
-main()
+io.on("connection", (socket) => {
+   console.log("Connected to socket.io");
+   socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
+   });
+
+   socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User Joined Room: " + room);
+   });
+   socket.on("typing", (room) => socket.in(room).emit("typing"));
+   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+   socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
+
+      if (!chat.users) return console.log("chat.users not defined");
+
+      chat.users.forEach((user) => {
+         if (user._id == newMessageRecieved.sender._id) return;
+
+         socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+   });
+
+   socket.off("setup", () => {
+      console.log("USER DISCONNECTED");
+      socket.leave(userData._id);
+   });
+});
